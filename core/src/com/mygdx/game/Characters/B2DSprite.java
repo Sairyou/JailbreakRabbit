3230022f787fdffc3f6c507ec5d45df24a340770
shadowraive <coolcliff892@Yahoo.com>
2017-02-10 13:28:29 -0600

    updated map

--- core/src/com/mygdx/game/Characters/B2DSprite.java
+++ core/src/com/mygdx/game/Characters/B2DSprite.java
@@ -1,6 +1,10 @@
 package com.mygdx.game.Characters;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Body;
+
+import static com.mygdx.game.MyGdxGame.PPM;
 
 /**
  * Created by Clifford Hill on 2/1/2017.
@@ -8,10 +12,18 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 public class B2DSprite {
     protected Body body;
+    protected Texture texture;
     protected float width;
     protected float height;
 
+
     public B2DSprite(Body body){
         this.body = body;
+    }
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(texture, (body.getPosition().x * PPM - width / 2),
+                (int) (body.getPosition().y * PPM - height / 2));
+        sb.end();
     }
 }
